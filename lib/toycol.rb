f4b0747effd89ef65645b94aa81802c0148a363c
Misaki Shioi <shioi.mm@gmail.com>
2021-07-15 20:56:47 +0900

    Add Handler & Proxy

--- lib/toycol.rb
+++ lib/toycol.rb
@@ -2,6 +2,11 @@
 
 require_relative "toycol/const"
 require_relative "toycol/protocol"
+require_relative "toycol/proxy"
+require_relative "rack/handler/toycol"
+
+Dir["#{FileUtils.pwd}/Protocolfile*"].sort.each { |f| load f }
+
 require_relative "toycol/version"
 
 module Toycol
