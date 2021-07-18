acb7277acbd86a7428c60c26c984f4e0b89f16d3
Misaki Shioi <shioi.mm@gmail.com>
2021-07-18 15:55:19 +0900

    Make sub commands available & Add -p option to client command

--- lib/toycol/command.rb
+++ lib/toycol/command.rb
@@ -5,23 +5,54 @@ require_relative "./client"
 
 module Toycol
   class Command
-    module Options
-      def self.parse!(argv)
-        opt_parser = OptionParser.new do |opts|
-          opts.banner = "Usage: toycol [options]"
+    class Options
+      class << self
+        def parse!(argv)
+          options            = {}
+          option_parser      = create_option_parser
+          sub_command_parser = create_sub_command_parser
 
-          opts.on("-c", "--client=REQUEST_MESSGAGE", "Sent request message to server") do |request_message|
-            ::Toycol::Client.execute!(request_message)
+          begin
+            option_parser.order!(argv)
+            options[:command]         = argv.shift
+            options[:request_message] = argv.shift if %w[client c].include? options[:command]
+
+            sub_command_parser[options[:command]].parse!(argv)
+          rescue OptionParser::MissingArgument, OptionParser::InvalidOption, ArgumentError => e
+            abort e.message
           end
 
-          opts.on("-v", "--version", "Show Toycol version") do
-            opts.version = Toycol::VERSION
-            puts opts.ver
-            exit
+          options
+        end
+
+        def create_option_parser
+          OptionParser.new do |opt|
+            opt.banner = "Usage: toycol [options]"
+
+            opt.on("-v", "--version", "Show Toycol version") do
+              opt.version = Toycol::VERSION
+              puts opt.ver
+              exit
+            end
           end
         end
 
-        opt_parser.parse!(argv)
+        def create_sub_command_parser
+          sub_command_parser = Hash.new { |_k, v| raise ArgumentError, "'#{v}' is not sub command" }
+          sub_command_parser["client"] = client_option_parser
+          sub_command_parser["c"]      = client_option_parser
+          sub_command_parser
+        end
+
+        private
+
+        def client_option_parser
+          OptionParser.new do |opt|
+            opt.on("-p=PORT_NUMBER", "--port=PORT_NUMBER", "Set port number") do |n|
+              ::Toycol::Client.port = n
+            end
+          end
+        end
       end
     end
 
@@ -34,7 +65,12 @@ module Toycol
     end
 
     def execute
-      Options.parse!(@argv)
+      options = Options.parse!(@argv)
+      command = options.delete(:command)
+
+      case command
+      when "client", "c" then ::Toycol::Client.execute!(options[:request_message])
+      end
     end
   end
 end
