dfb6521f7e26f475241611cadf1df5a242ba7480
Misaki Shioi <shioi.mm@gmail.com>
2021-07-19 21:33:33 +0900

    Add test for operation verification of proxy server

--- test/proxy_integration_test.rb
+++ test/proxy_integration_test.rb
@@ -21,9 +21,39 @@ class ProxyIntegrationTest < Minitest::Test
       Process.setpgrp # To detach from current process group
       ::Rack::Handler::Toycol.run(TestApp.new, { Port: 12_345 })
     end
+
+    sleep 1 # Wait for server to launch
   end
 
   def teardown
     Process.kill(:INT, -Process.getpgid(@server_pid))
+  end
+
+  EXPECTED_RESPONSE_MESSAGE_FOR_GET = <<~MESSAGE
+    HTTP/1.1 200 OK\r
+    Content-Type: text/html\r
+    Content-Length: 17\r
+    \r
+    Response for GET
+  MESSAGE
+
+  EXPECTED_RESPONSE_MESSAGE_FOR_OTHER = <<~MESSAGE
+    HTTP/1.1 600 This is a test status code\r
+    Content-Type: text/html\r
+    Content-Length: 19\r
+    \r
+    Response for OTHER
+  MESSAGE
+
+  def test_that_it_returns_response_message
+    assert_equal(
+      EXPECTED_RESPONSE_MESSAGE_FOR_GET,
+      execute_client("GET /posts", port: 12_345)
+    )
+
+    assert_equal(
+      EXPECTED_RESPONSE_MESSAGE_FOR_OTHER,
+      execute_client("OTHER /posts", port: 12_345)
+    )
   end
 end
