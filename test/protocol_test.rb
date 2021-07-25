b8a0a7cbbd57fc60a24dd150a1e0560ccaed5305
Misaki Shioi <shioi.mm@gmail.com>
2021-07-25 21:33:07 +0900

    Add DuplicateProtocolError for protocols with the same name

--- test/protocol_test.rb
+++ test/protocol_test.rb
@@ -5,29 +5,40 @@ require_relative "test_helper"
 class ProtocolTest < Minitest::Test
   include Helper
 
-  def setup
-    setup_test_protocol
-  end
-
   def test_that_it_defines_protocol_name
-    Toycol::Protocol.run!("GET /posts?user_id=1")
+    Toycol::Protocol.define(:protocol_test_define1) { "protocol_test_define1" }
+    Toycol::Protocol.use(:protocol_test_define1)
 
     assert_equal(
-      :test,
-      Toycol::Protocol.instance_variable_get("@definements").keys.first
+      "protocol_test_define1",
+      Toycol::Protocol.instance_variable_get("@definements")[:protocol_test_define1].call
+    )
+  end
+
+  def test_that_it_returns_error_when_there_are_two_duplucate_protocols
+    error = assert_raises Toycol::DuplicateProtocolError do
+      Toycol::Protocol.define(:protocol_test_define2) { "protocol_test_define2_1" }
+      Toycol::Protocol.define(:protocol_test_define2) { "protocol_test_define2_2" }
+    end
+
+    assert_equal(
+      "protocol_test_define2 protocol has already been defined",
+      error.message
     )
   end
 
   def test_that_it_uses_specified_protocol_name
-    Toycol::Protocol.run!("GET /posts?user_id=1")
+    Toycol::Protocol.define(:protocol_test_use1) { "protocol_test_use1" }
+    Toycol::Protocol.use(:protocol_test_use1)
 
     assert_equal(
-      :test,
+      :protocol_test_use1,
       Toycol::Protocol.instance_variable_get("@protocol_name")
     )
   end
 
   def test_that_it_returns_request_path
+    setup_test_protocol
     Toycol::Protocol.run!("GET /posts")
 
     assert_equal(
@@ -37,6 +48,7 @@ class ProtocolTest < Minitest::Test
   end
 
   def test_that_it_returns_error_when_size_is_too_long
+    setup_test_protocol
     Toycol::Protocol.run!("GET /#{"posts" * 2048}")
 
     error = assert_raises Toycol::UnauthorizeError do
@@ -50,7 +62,12 @@ class ProtocolTest < Minitest::Test
   end
 
   def test_that_it_returns_error_when_contains_unauthorized_character
-    Toycol::Protocol.define(:test) { request.path { "/p+o+s+t+s" } }
+    Toycol::Protocol.define(:protocol_test_request_path1) do
+      request.path { "/p+o+s+t+s" }
+      request.http_method { |message| /^(?<http_method>\w+)\b/.match(message) { |m| m[:http_method] }.upcase }
+    end
+
+    Toycol::Protocol.use(:protocol_test_request_path1)
     Toycol::Protocol.run!("GET /p+o+s+t+s")
 
     error = assert_raises Toycol::UnauthorizeError do
@@ -64,6 +81,7 @@ class ProtocolTest < Minitest::Test
   end
 
   def test_that_it_returns_request_method
+    setup_test_protocol
     Toycol::Protocol.run!("GET /posts")
 
     assert_equal(
@@ -73,6 +91,7 @@ class ProtocolTest < Minitest::Test
   end
 
   def test_that_it_returns_error_when_is_not_defined
+    setup_test_protocol
     Toycol::Protocol.run!("UNDEFINED /posts")
 
     error = assert_raises Toycol::UndefinementError do
@@ -86,6 +105,7 @@ class ProtocolTest < Minitest::Test
   end
 
   def test_that_it_returns_query
+    setup_test_protocol
     Toycol::Protocol.run!("GET /posts?user_id=1")
 
     assert_equal(
@@ -95,6 +115,7 @@ class ProtocolTest < Minitest::Test
   end
 
   def test_that_it_returns_input
+    setup_test_protocol
     Toycol::Protocol.run!("POST /posts\r\nThis is test.")
 
     assert_equal(
@@ -104,6 +125,7 @@ class ProtocolTest < Minitest::Test
   end
 
   def test_that_it_returns_default_http_status_message
+    setup_test_protocol
     Toycol::Protocol.run!("GET /posts")
 
     assert_equal(
@@ -113,6 +135,7 @@ class ProtocolTest < Minitest::Test
   end
 
   def test_that_it_returns_custom_http_status_message
+    setup_test_protocol
     Toycol::Protocol.run!("GET /posts")
 
     assert_equal(
@@ -122,6 +145,7 @@ class ProtocolTest < Minitest::Test
   end
 
   def test_that_it_returns_error_when_status_code_is_not_defined
+    setup_test_protocol
     Toycol::Protocol.run!("GET /posts")
 
     error = assert_raises Toycol::HTTPError do
